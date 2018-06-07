8aefcfd0105779ee2dc4a1120db304c75bf8a070
yutaro1031 <yutaro50@gmail.com>
2018-06-07 19:35:52 +0900

    otameshi

--- table/models.py
+++ table/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
+saa
 
 
 
